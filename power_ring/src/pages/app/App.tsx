3b46e59c86c07d424e64d2ae7394e03599bf979d
Guilherme Alves <guilhermesaraivaalves@gmail.com>
2023-11-08 19:46:13 -0400

    adding papaya page

--- power_ring/src/pages/app/App.tsx
+++ power_ring/src/pages/app/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import { Main } from '../main/main';
 import { About } from '../about/about';
-import { BrowserRouter, Routes, Route, Link, NavLink } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
           <nav>
           <NavLink to='/'>Main</NavLink>
           <NavLink to='/About/'>About</NavLink>
+          <NavLink to='/papaya/'>papaya</NavLink>
           </nav>
         </header>
 
@@ -18,6 +19,17 @@ function App() {
           <Routes>
             <Route index element={<Main/>} />
             <Route path='/About/' element={<About/>} /> 
+            <Route path ='/papaya' element ={ <>
+              <div style={{
+                width:'100vw',
+                height:'100vh',
+                background:'linear-gradient(216deg, rgba(36,0,1,1) 0%, rgba(68,5,5,1) 32%, rgba(73,121,9,0.44021358543417366) 57%, rgba(224,0,185,1) 77%, rgba(0,212,255,1) 100%)',
+                marginTop: -20
+
+              }} >
+
+              </div>
+            </>} />
           </Routes>
         </main>
       </div>
